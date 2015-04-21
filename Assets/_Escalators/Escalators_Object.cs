ae7daeaa8c612e521b5d3df7be1e00033abacad4
lbarrett93 <lbarrett1993@gmail.com>
2015-04-21 12:40:48 -0400

    Commit fix as of 4/21

---
+++ Assets/_Escalators/Escalators_Object.cs
@@ -0,0 +1,20 @@
+﻿// Haikun Huang
+using UnityEngine;
+using System.Collections;
+
+public class Escalators_Object : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+
+}
